1eee13ad15605a00024defc2b9ab9fecacde1bb0
sowndarya190 <sowndaryachandrappa@gmail.com>
2025-08-13 01:03:23 +0530

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,11 +1,1 @@
-// Sample code for a "balloon release" effect
-function releaseBalloons() {
-  const container = document.getElementById('balloon-container');
-  for (let i = 0; i < 20; i++) {
-    const balloon = document.createElement('div');
-    balloon.className = 'balloon';
-    balloon.style.left = `${Math.random() * 100}%`;
-    balloon.style.animationDuration = `${5 + Math.random() * 5}s`;
-    container.appendChild(balloon);
-  }
-}
+
